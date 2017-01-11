5c2ac22e8ee18c660f4877e09c509990b8e114b4
CharleyMcLean <charleyaustinmclean@gmail.com>
2017-01-11 11:49:47 -0800

    Removed unnecessary imports.  Fixed indentation.

--- random.py
+++ random.py
@@ -1,18 +1,16 @@
-import randomorg as ro
 import requests
-import urllib2
-import urllib
+
 
 def generate_rand_ints(num, mini, maxi, col, base, email, format='plain',
                        rnd='new', url='https://www.random.org/integers/'):
     # all inputs need to be in string format
     payload = {'num': num,
-                'min': mini,
-                'max': maxi,
-                'col': col,
-                'base': base,
-                'format': format,
-                'rnd': rnd}
+               'min': mini,
+               'max': maxi,
+               'col': col,
+               'base': base,
+               'format': format,
+               'rnd': rnd}
 
     headers = {'user-agent': email}
 
