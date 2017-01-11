3f75df4dccf28783b407d0c3f01cffff1110be89
CharleyMcLean <charleyaustinmclean@gmail.com>
2017-01-11 11:47:54 -0800

    Generating random numbers using random.org api.

---
+++ random.py
@@ -0,0 +1,22 @@
+import randomorg as ro
+import requests
+import urllib2
+import urllib
+
+def generate_rand_ints(num, mini, maxi, col, base, email, format='plain',
+                       rnd='new', url='https://www.random.org/integers/'):
+    # all inputs need to be in string format
+    payload = {'num': num,
+                'min': mini,
+                'max': maxi,
+                'col': col,
+                'base': base,
+                'format': format,
+                'rnd': rnd}
+
+    headers = {'user-agent': email}
+
+    r = requests.get(url, params=payload, headers=headers)
+
+    # split on the new line
+    return r.text.split('\n')
